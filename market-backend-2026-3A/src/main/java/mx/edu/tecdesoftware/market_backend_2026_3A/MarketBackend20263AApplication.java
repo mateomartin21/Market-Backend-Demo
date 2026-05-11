d6e3608b927efa26463699f105835199b5d11ad8
mateomartin21 <mateo.martin@tecdesoftware.edu.mx>
2026-05-11 08:26:45 -0600

    chore: initial Spring Boot project setup

---
+++ market-backend-2026-3A/src/main/java/mx/edu/tecdesoftware/market_backend_2026_3A/MarketBackend20263AApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_2026_3A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackend20263AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBackend20263AApplication.class, args);
+	}
+
+}
---
+++ market-backend-2026-3A/src/test/java/mx/edu/tecdesoftware/market_backend_2026_3A/MarketBackend20263AApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.market_backend_2026_3A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackend20263AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
